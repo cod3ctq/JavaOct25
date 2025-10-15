b7e91c0db58eb374b8a270168a9e2d1ec73acb57
abrahamsaulsotoreyes-spec <abrahamsaulsotoreyes@gmail.com>
2025-10-15 09:06:39 -0600

    Proyecto Herencia ASSR

---
+++ herencia_assr/src/Celular.java
@@ -0,0 +1,83 @@
+public class Celular {
+    String marca;
+    String modelo;
+    String color ;
+    String tamaño;
+
+//constructor vacio
+    public Celular(){}
+
+    public Celular(String marca, String modelo, String color, String tamaño) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.tamaño = tamaño;
+    }
+    //get y set
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
+    }
+//tostring
+    @Override
+    public String toString() {
+        return "Celular{" +
+                "marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", color='" + color + '\'' +
+                ", tamaño='" + tamaño + '\'' +
+                '}';
+    }
+
+
+    public void llamar(String numero){
+
+        System.out.println("Llamando al numero"+numero);
+
+    }
+
+    public void mandarMensaje(String numero,String mensaje){
+
+        System.out.println("Enviando :"+mensaje+" al numero: "+numero);
+
+    }
+
+    public void alarma(String hora){
+
+        System.out.println("La alarma sonara a las "+hora);
+
+    }
+
+
+
+
+}
+
